c23c5b8ab9ecd830919ce59cb29655a721f0404a
Riviere Kevin <sudokeys30@gmail.com>
2021-08-15 10:50:14 -0400

    Ajout nouvelles idées

--- Divers idées/Animation de page/Idée 1/app.js
+++ Divers idées/Animation de page/Idée 1/app.js
@@ -9,13 +9,13 @@ const tl = new TimelineMax();
 tl.fromTo(hero, 1, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
     .fromTo(
         hero,
-        1.8,
+        1.2,
         { width: "100%" },
         { width: "80%", ease: Power2.easeInOut }
 )
 .fromTo(
     slider,
-    1.6,
+    1.1,
     { x: "-100%" },
     { x: "0%", ease: Power2.easeInOut },
     "-=1.4"
